103655e6f451391d123febaf36c3c9503a67e7fe
lmalla <53437791+lmalla@users.noreply.github.com>
2022-02-11 15:32:12 -0500

    Final Commit

--- Lekh/config.py
+++
@@ -1,5 +0,0 @@
-api_key = "HNROimQHBPL-tJvXC1zOvhzdO7JGsZTnLGFzLiqT38tG86j1U-lb4XsGsatznGmgkLQVsW7cIqmYShZLZh0ZquUJj8kVgK6eVExu91Je5u-Lwb1B0WcOZWWH9G_8YXYx"
-
-gkey = "AIzaSyB7HMEmF602ZHtj9LRsILbnXZRHxDQxgdk"
-
-
